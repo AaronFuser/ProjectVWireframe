76b2a46a747877f3252131618052adfa0610d53f
aaa <A.fuser04@gmail.com>
2021-04-08 12:19:52 +1000

    uPDATED file names and additional build versions (Buildv1)

---
+++ ProjectV/UX vodka/Assets/scripts/ItemController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public DisplayLogic[] MyDisplays;
+    public ScriptableItem[] Possibleitems;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < MyDisplays.Length; i++)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                MyDisplays[i]._items.Add(Possibleitems[0]);
+            }
+            
+            MyDisplays[i].SetDisplay();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+}
---
+++ ProjectV/UX vodka/Assets/scripts/ScriptableTransfer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "NewTransfer", menuName = "ScriptableObject/Transfer")]
+public class ScriptableTransfer : ScriptableObject
+{
+    public ScriptableItem _value;
+
+    private void OnEnable()
+    {
+        _value = null;
+    }
+}
